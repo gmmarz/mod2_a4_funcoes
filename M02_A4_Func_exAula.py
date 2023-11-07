7ba7244dd93fdabe1eeed53cf056ea8e851f4226
guilherme <guilhermemarzano@gmail.com>
2023-11-06 22:04:15 -0300

    fim aula

--- M02_A4_Func_exAula.py
+++ M02_A4_Func_exAula.py
@@ -2,6 +2,10 @@
 
 #Quando adicionado o tipa da variável na frente, isso fará que ao chamar a função apareça dicas sobre o que
 #a função espera receber. Porem não dará erro caso a função receba uma string ou algum tipo diferente.
+def imprime_menu():
+    print('-'*30)
+    print('Menu')
+    print('-'*30)
 
 def calcular_IMC(peso:float|int, altura:float|int)->float:
     imc = round(peso/altura**2,2)
@@ -14,7 +18,7 @@ peso = float(input('Digite seu peso: '))
 altura = float(input('Digite sua altura: '))
 
 result_imc = calcular_IMC(peso,altura)
-
+imprime_menu()
 print(f'O seu IMC = {result_imc}')
 
 
--- Readme.txt
+++ Readme.txt
@@ -3,3 +3,11 @@ Aula 4 primeiro commit
 Dica para react - para poder utilizar react é necessário ter algum conhecimento
 em css, html e javascript.
 
+Recomendação de exercicios 
+Site:
+python Brasil lista de exercicios.
+
+Site: Codewars
+Exercicios para python e outras linguagens. (codewars.com)
+
+dontpad = dontpad.com/inf219-0611
